28e7efc956de7233507fb9992ad48294166061b9
Hazem Ali <hazemali627@gmail.com>
2022-11-28 00:19:08 +0200

    token finished

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import RegisterView
+from .views import RegisterView,LogoutView
 
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- user_app/api/views.py
+++ user_app/api/views.py
@@ -2,13 +2,30 @@ from rest_framework.views import APIView
 from .serializers import RegisterSerializer
 from rest_framework.response import Response
 
+from rest_framework.authtoken.models import Token
+
+
+class LogoutView(APIView):
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response({'message': 'Logout Success'})
 
 
 class RegisterView(APIView):
 
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
+
         if serializer.is_valid():
-            serializer.save()
-        
-            return Response(serializer.data)
+            account = serializer.save()
+
+            data = {}
+            data['response'] = 'Success'
+            data['username'] = account.username
+            data['email'] = account.email
+
+            token, is_created = Token.objects.get_or_create(user=account)
+            data['token'] = token.key
+
+            return Response(data)
+        return Response(serializer.errors)
